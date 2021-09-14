aac76b837a3f40c138a81a886755dc73166d6344
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-09-14 15:39:44 -0700

    update mempool pool defaults
    8x for larger capacity for NEVM

--- core/tx_pool.go
+++ core/tx_pool.go
@@ -177,9 +177,9 @@ var DefaultTxPoolConfig = TxPoolConfig{
 	PriceBump:  10,
 
 	AccountSlots: 16,
-	GlobalSlots:  4096 + 1024, // urgent + floating queue capacity with 4:1 ratio
+	GlobalSlots:  32768 + 8192, // urgent + floating queue capacity with 4:1 ratio
 	AccountQueue: 64,
-	GlobalQueue:  1024,
+	GlobalQueue:  8192,
 
 	Lifetime: 3 * time.Hour,
 }
