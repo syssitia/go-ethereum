4f329d43f4026e9a2d7027e55cd75513dd913b1a
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2021-09-17 16:34:54 -0700

    Update json.go

--- rpc/json.go
+++ rpc/json.go
@@ -229,7 +229,19 @@ func (c *jsonCodec) writeJSON(ctx context.Context, v interface{}) error {
 		deadline = time.Now().Add(defaultWriteTimeout)
 	}
 	c.conn.SetWriteDeadline(deadline)
-	log.Warn("writeJSON", "len", len(v.(*jsonrpcMessage).Result))
+	jsonMsg := v.(*jsonrpcMessage)
+	count := 0
+	if jsonMsg != nil {
+		count = len(jsonMsg.Result)
+	} else {
+		jsonMsgArr := v.([]*jsonrpcMessage)
+		if jsonMsgArr != nil {
+			for _, msg := range jsonMsgArr {
+				count += len(msg.Result)
+			}
+		}
+	}
+	log.Warn("writeJSON", "count", count)
 	return c.encode(v)
 }
 
