b8730d88076fdcf88a52dcaa9ae60e93fa687f7f
JoeGruffins <34998433+JoeGruffins@users.noreply.github.com>
2022-04-12 17:36:29 +0900

    min gasprice of 5 wei

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -85,7 +85,7 @@ var Defaults = Config{
 	SnapshotCache:           102,
 	Miner: miner.Config{
 		GasCeil:  8000000,
-		GasPrice: big.NewInt(params.GWei),
+		GasPrice: big.NewInt(5*params.Wei),
 		Recommit: 3 * time.Second,
 	},
 	TxPool:        core.DefaultTxPoolConfig,
