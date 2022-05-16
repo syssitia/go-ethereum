56101ddece543ffe37b8b8ea9debc888c263660f
Håvard Anda Estensen <haavard.ae@gmail.com>
2022-05-16 11:59:35 +0200

    all: replace uses of ioutil with io and os (#24869)

--- core/tx_pool_test.go
+++ core/tx_pool_test.go
@@ -20,7 +20,6 @@ import (
 	"crypto/ecdsa"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"math/big"
 	"math/rand"
 	"os"
@@ -2257,7 +2256,7 @@ func testTransactionJournaling(t *testing.T, nolocals bool) {
 	t.Parallel()
 
 	// Create a temporary file for the journal
-	file, err := ioutil.TempFile("", "")
+	file, err := os.CreateTemp("", "")
 	if err != nil {
 		t.Fatalf("failed to create temporary journal: %v", err)
 	}
