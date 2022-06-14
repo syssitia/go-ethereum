246e2a409141ccf0b78157505c03088931a5b3c5
Ivan Aracki <aracki.ivan@gmail.com>
2022-06-14 10:24:29 +0200

    cmd/utils: update --ropsten description (#25078)

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -150,7 +150,7 @@ var (
 	}
 	RopstenFlag = cli.BoolFlag{
 		Name:  "ropsten",
-		Usage: "Ropsten network: pre-configured proof-of-work test network",
+		Usage: "Ropsten network: pre-configured proof-of-stake test network",
 	}
 	SyscoinFlag = cli.BoolFlag{
 		Name:  "syscoin",
