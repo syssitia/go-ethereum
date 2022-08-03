14bfa21168aa0a26229d4873c573f5dce02cb0c8
Tristan-Wilson <87238672+Tristan-Wilson@users.noreply.github.com>
2022-08-03 07:50:12 -0700

    node, rpc: add ReadHeaderTimeout config option (#25338)

--- rpc/http.go
+++ rpc/http.go
@@ -87,6 +87,14 @@ type HTTPTimeouts struct {
 	// ReadHeaderTimeout. It is valid to use them both.
 	ReadTimeout time.Duration
 
+	// ReadHeaderTimeout is the amount of time allowed to read
+	// request headers. The connection's read deadline is reset
+	// after reading the headers and the Handler can decide what
+	// is considered too slow for the body. If ReadHeaderTimeout
+	// is zero, the value of ReadTimeout is used. If both are
+	// zero, there is no timeout.
+	ReadHeaderTimeout time.Duration
+
 	// WriteTimeout is the maximum duration before timing out
 	// writes of the response. It is reset whenever a new
 	// request's header is read. Like ReadTimeout, it does not
@@ -104,6 +112,7 @@ type HTTPTimeouts struct {
 // configuration is not provided.
 var DefaultHTTPTimeouts = HTTPTimeouts{
 	ReadTimeout: 30 * time.Second,
+	ReadHeaderTimeout: 30 * time.Second,
 	// SYSCOIN
 	WriteTimeout: 300 * time.Second,
 	IdleTimeout:  120 * time.Second,
